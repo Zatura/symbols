5a7623915394e46b809126919585612796ca29a8
Matheus Tura <matheus.tura@protonmail.com>
2023-04-12 17:48:03 -0300

    Add new listings, Update symbols info, Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = open('README.md').read()
 
 setup(
     name='symbols',
-    version='0.4.1',
+    version='0.4.2',
     author='Matheus Tura',
     author_website='zatura.me',
     packages=find_packages(),
